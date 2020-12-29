9b68a810cabd1f263546c47ea86ccc5ce34762da
lsk <longsk@casc.ac.cn>
2020-12-29 11:40:17 +0800

    first commit

---
+++ babel-2020-12-29/src/index.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+
+const index = {
+    list: [23, 4, 'hello world', 'dog233'],
+    data: {
+        name: '小明',
+        age: 23,
+    },
+    init() {
+        const _this = this;
+
+        _this.changeList();
+    },
+    // changeList: function() {
+    changeList() {
+        const _this = this;
+        console.log('begin：', _this.list);
+        let arr = [];
+
+        arr = _this.list.find(item => {
+            return item > 10;
+        });
+
+        console.log('end', arr);
+        console.log('是否包含23:', _this.list.includes(23));
+        let data = _.assign(_this.data, {gender: '男'});
+        console.log(data);
+    },
+};
+index.init();
